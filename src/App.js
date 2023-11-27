eb47119c61ec76457ab2c56e6e042c5922e9dcc1
Chirag Mittal <chiragmittal36269@gmail.com>
2023-11-27 23:51:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AddTask from "./Components/AddTask";
+import EditTask from "./Components/EditTask";
+import TaskList from "./Components/TaskList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <TaskList />
+            <AddTask />
+            <EditTask />
+        </div>
+    );
+};
 
 export default App;
